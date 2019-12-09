169184e9c5747d6d43e3b80f3c919b1934250d07
Mike Ballard <mballar@columbus.rr.com>
2019-12-09 13:20:15 -0500

    Added Model Classes

--- vue/backend/database/schema.sql
+++ vue/backend/database/schema.sql
@@ -10,16 +10,15 @@ CREATE TABLE users (
   role varchar(255) NOT NULL default('user')
 );
 
-CREATE TABLE person (
-    person_id serial,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(25) NOT NULL,
-    date_of_birth date,
-    application_id int,
-    primary_applicant boolean,
+CREATE TABLE address (
+    address_id serial,
+    address_line1 VARCHAR(40) NOT NULL,
+    address_line2 VARCHAR(40),
+    city VARCHAR(25) NOT NULL,
+    state VARCHAR(15) NOT NULL,
+    zip_code int,
 
-    constraint pk_person_id primary key (person_id),
-    constraint fk_application_id foreign key (application_id) references application (application_id)
+    constraint pk_address_id primary key (address_id)
 );
 
 CREATE TABLE application (
@@ -33,17 +32,16 @@ CREATE TABLE application (
     constraint fk_address_id foreign key (address_id) references address (address_id)
 );
 
-CREATE TABLE address (
-    address_id serial,
-    address_line1 VARCHAR(40) NOT NULL,
-    address_line2 VARCHAR(40),
-    city VARCHAR(25) NOT NULL,
-    state VARCHAR(15) NOT NULL,
-    zip_code int,
+CREATE TABLE person (
+    person_id serial,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    date_of_birth date,
+    application_id int,
+    primary_applicant boolean,
 
-    constraint pk_address_id primary key (address_id)
+    constraint pk_person_id primary key (person_id),
+    constraint fk_application_id foreign key (application_id) references application (application_id)
 );
-
-
 
 COMMIT TRANSACTION;
