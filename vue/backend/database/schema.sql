137472197c1550981c8090110c6ea77b8d0d9a0e
Kelsey Wooster <kelsey.wooster@gmail.com>
2019-12-09 11:57:16 -0500

    Updated schema.sql with scripts to create person, application, and address tables

--- vue/backend/database/schema.sql
+++ vue/backend/database/schema.sql
@@ -10,4 +10,41 @@ CREATE TABLE users (
   role varchar(255) NOT NULL default('user')
 );
 
+CREATE TABLE person (
+    person_id serial,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    date_of_birth date,
+    application_id int,
+
+    constraint pk_person_id primary key (person_id),
+    constraint fk_application_id foreign key (application_id) references application (application_id)
+);
+
+CREATE TABLE application (
+    application_id serial,
+    main_contact_id int NOT NULL,
+    address_id int,
+    email VARCHAR(40),
+    phone_number int,
+    status VARCHAR(8),
+
+    constraint pk_application_id primary key (application_id),
+    constraint fk_main_contact_id foreign key (main_contact_id) references person (person_id),
+    constraint fk_address_id foreign key (address_id) references address (address_id)
+);
+
+CREATE TABLE address (
+    address_id serial,
+    address_line1 VARCHAR(40) NOT NULL,
+    address_line2 VARCHAR(40),
+    city VARCHAR(25) NOT NULL,
+    state VARCHAR(15) NOT NULL,
+    zip_code int,
+
+    constraint pk_address_id primary key (address_id)
+);
+
+
+
 COMMIT TRANSACTION;
