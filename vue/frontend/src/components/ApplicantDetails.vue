3d6bbf5fd658e3490964d086af411d08c1fa8d1a
Kelsey Wooster <kelsey.wooster@gmail.com>
2019-12-11 11:45:51 -0500

    Added filters, navigation components. Modified applicant table and applicant list to reflect wireframe HTML and CSS. Modified App.vue to display nav bar and match wireframe

---
+++ vue/frontend/src/components/ApplicantDetails.vue
@@ -0,0 +1,148 @@
+<template>
+            <div class="details">
+            <div class="details-top">
+                <h1>Kelsey Wooster</h1>
+                <div class="control">
+                    <button>Edit</button>
+                    <button>Recent Changes</button>
+                </div>
+            </div>
+            <h3>Application #123456<br>Account #0000335</h3>
+            <div class="row">
+                <p class="title"><b>Preferred Name</b></p>
+                <hr>
+                <p>Kelsey</p>
+            </div>
+            <div class="row">
+                <p class="title"><b>Date of Birth</b></p>
+                <hr>
+                <p>8/26/1987</p>
+            </div>
+            <div class="row">
+                <p class="title">Email</p>
+                <hr>
+                <p>kelsey.wooster@gmail.com</p>
+            </div>
+            <div class="row">
+                <p class="title">Phone Number</p>
+                <hr>
+                <p>(614) 226-2599</p>
+            </div>
+            <div class="row">
+                <p class="title">Congregation</p>
+                <hr>
+                <p>Church of the Flying Spaghetti Monster</p>
+            </div>
+            <div class="row">
+                <p class="title">T-Shirt Size</p>
+                <hr>
+                <p>XS</p>
+            </div>
+            <div class="row">
+                <p class="title">Dietary Preference</p>
+                <hr>
+                <p>Omnivore</p>
+            </div>
+            <div class="row">
+                <p class="title">Dietary Restrictions</p>
+                <hr>
+                <p>Gluten-Free (allergy)</p>
+            </div>
+            <div class="row">
+                <p class="title">Mobility Issues</p>
+                <hr>
+                <p>N/A</p>
+            </div>
+            <div class="row">
+                <p class="title">Medical Concerns</p>
+                <hr>
+                <p>N/A</p>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'applicant-details',
+    data: {
+        application: { }
+    }
+}
+</script>
+
+<style>        
+        .details-top>h1 {
+            font-family: 'Playfair Display', serif;
+            font-style: italic;
+            font-size: 2.3em;
+            margin: 5px 0px 20px -15px;
+        }
+        
+        .details {
+            flex-basis: 70%;
+        }
+        
+        .details-top>h1 {
+            display: inline-block;
+            width: 65%;
+        }
+        
+        .details-top .control {
+            display: inline-block;
+            width: auto;
+            margin-right: 20px;
+        }
+        
+        .details>h3 {
+            font-family: 'Playfair Display', serif;
+            font-style: italic;
+            font-size: 1em;
+            margin-top: -10px;
+        }
+        
+        .details .row {
+            display: inline-block;
+            width: 40%;
+            margin: 0px 10px 15px 0px;
+        }
+        
+        .details .row>p {
+            display: block;
+        }
+        
+        .details hr {
+            border-top: 1px solid #792359;
+            margin: 10px 0px 15px 0px;
+        }
+        
+        .details>h3,
+        .row>h3 {
+            font-family: 'Playfair Display', serif;
+            font-style: italic;
+            font-size: 1em;
+        }
+        
+        .row>p {
+            font-size: 1.2em;
+            margin: auto;
+            padding: 4px;
+        }
+        
+        .row .title {
+            font-size: 1.2em;
+            text-transform: uppercase;
+        }
+        
+        .row hr {
+            border-top: 1px solid #792359;
+            margin: 0px 0px 0px 0px;
+        }
+        
+        .row>h3 {
+            margin-bottom: -4px;
+        }
+        
+        .subdata {
+            padding-left: 15px;
+        }
+</style>
