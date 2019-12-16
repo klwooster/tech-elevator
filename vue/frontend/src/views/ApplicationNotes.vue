3acd76a314caaf61a2e972529b7666ee2601af9f
Jenny Dihrkop <jennydihrkop@gmail.com>
2019-12-16 12:50:53 -0500

    working on plugging notes api into vue

--- vue/frontend/src/views/ApplicationNotes.vue
+++ vue/frontend/src/views/ApplicationNotes.vue
@@ -1,36 +1,44 @@
 <template>
   <div id="main">
     <div v-for="note in noteData" v-bind:key="note.noteId">
-
-    <note-details v-bind:noterecord="note"/>
+      <note-details v-bind:noterecord="note"/>
     </div>
   </div>
 </template>
 
 <script>
 
-import NoteDetails from '@/components/NoteDetails.vue'
+import NoteDetails from '@/components/NoteDetails.vue';
+import APIService from '@/service/APIService';
 
 
 export default {
 name: 'application-note',
 components: {
-NoteDetails
+  NoteDetails
+},
+props: {
+  id: {},
+  isInEditMode: false
 },
 data() {
 return {
-noteData: [{
-noteId: "",
-noteBody: "",
-createDate: "",
-applicationId: ""
-}]
+  noteData: [{
+    noteId: "",
+    noteBody: "",
+    createDate: "",
+    applicationId: ""
+  }]
 }
 },
 methods: { 
   getNotes(id) {
-    
+    APIService.getNotesByApplication(id);
+    // .then(applicationdata => this.applicationdata = applicationdata);
   }
+},
+created() {
+  getNotes(this.id);
 }
 };
 </script>
